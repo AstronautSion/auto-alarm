c27e22157c6ed2129d351e64f518157f9a834fed
AstronautSion <astronaut.sion@gmail.com>
2021-07-15 17:30:27 +0900

    remove body-parser

--- bot.js
+++ bot.js
@@ -11,16 +11,25 @@ dotenv.config();
 
 var express = require('express');
 var app = express();
+var session = require('express-session');
+var fs = require("fs")
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
- 
+
+
 var server = app.listen(3000, function(){
  console.log("Express server has started on port 3000")
 });
 
 app.use(express.static('public'));
+app.use(session({
+ secret: '@#@$MYSIGN#@$#$',
+ resave: false,
+ saveUninitialized: true
+}));
+
 let router = require('./router/main')(app, fs, botEvent);
 
 
