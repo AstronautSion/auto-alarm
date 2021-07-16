9e96649d7bb811d60b2b95796ac7094dc1a82b49
AstronautSion <astronaut.sion@gmail.com>
2021-07-16 13:10:01 +0900

    test pwd

--- bot.js
+++ bot.js
@@ -23,9 +23,9 @@ app.get('/', (req, res) => {
 
 app.get('/addEvent', (req, res) => {
 	if(req.query.pwd != '' && typeof(req.query.pwd) == 'string'){
-		console.log(req.query.pwd ,process.env.PWD)
+		console.log(String(req.query.pwd) , String(process.env.PWD))
 
-		if(req.query.pwd === process.env.PWD){
+		if(String(req.query.pwd) === String(process.env.PWD) ){
 			botEvent();
 			res.json({msg: '업데이트를 진행합니다.\n 업데이트 반영은 1~2분 정도 소요 될 수 있습니다.'})
 		}else{
